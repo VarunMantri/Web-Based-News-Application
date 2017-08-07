a8344a2833c46cac3e504159b65ce25b2e90c2d5
Varun Rajiv Mantri <mantri.varun16@gmail.com>
2017-08-07 17:49:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+//author Varun Rajiv Mantri
+
+
+
+// Main Server file 
+
 var express=require("express");
 var restHandler=require("./restHandle.js");
 var config=require("./config.js");
 var app=express();
 
+//setting the default view engine as 'ejs'
 app.set("view engine","ejs");
 app.use("images",express.static(__dirname+"/img"));
+//setting up the location information for styles content
 app.use("/styles",express.static(__dirname + "/styles"));
+//mounting the default route 
 app.use("/",restHandler);
+//listening on default port 3000 and hostname:localhost
 app.listen(config.port,config.hostname,function(){
     console.log("Server running on http://"+config.hostname+":"+config.port);
 });
