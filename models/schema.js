84644d4b3ce219ed08fadcab7a33be9342c89246
Varun Rajiv Mantri <mantri.varun16@gmail.com>
2017-08-07 17:36:00 -0400

    Add files via upload

--- models/schema.js
+++ models/schema.js
@@ -1,1 +1,16 @@
+var mongoose=require("mongoose");
+var schema=mongoose.Schema;
 
+var uris=new schema({
+    uri:{type:String}
+});
+
+var data=new schema(
+{
+    _id:{type:String},
+    data:[uris]
+}
+);
+
+var mod=mongoose.model("newsData",data);
+module.exports=mod;
